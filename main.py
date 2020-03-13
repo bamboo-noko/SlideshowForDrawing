bb571d08bdb23e39396ac1d69108a711d7dd0ddc
bamboo-noko <plumhearts.yuki@gmail.com>
2020-03-13 23:06:53 +0900

    add pause

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import filedialog
 import datetime
 import json
 import ast
+import re
 from PIL import Image, ImageTk, ImageOps
 
 class History():
@@ -25,7 +26,11 @@ class Application(tk.Frame):
         self.index = 0
         self.history = []
         self.create_menu()
+        self.elapsed_time = 0
+        self.tick = 1
         self.create_widgets()
+        self.after_id = 0
+        self.is_puase = False
 
     def create_menu(self):
         menu = tk.Menu(self.master)
@@ -48,8 +53,8 @@ class Application(tk.Frame):
         self.select_directory_frame.pack(fill="x")
         self.input_directory = tk.Entry(self.select_directory_frame, width=200)
         self.input_directory.pack(side="left")
-        self.button_read_directory = tk.Button(self.select_directory_frame, text="read", command=self.read_image)
-        self.button_read_directory.pack(side="left")
+        self.button_open_directory = tk.Button(self.select_directory_frame, text="open", command=self.open_image)
+        self.button_open_directory.pack(side="left")
 
         self.practice_menu_frame = tk.Frame(self)
         self.practice_menu_frame.pack(fill="x")
@@ -73,52 +78,140 @@ class Application(tk.Frame):
         self.button_frame.pack(fill="x")
         self.button_start = tk.Button(self.button_frame, text="start", command=self.start)
         self.button_start.pack(side="left")
+        self.button_pause = tk.Button(self.button_frame, text="pause", command=self.pause)
+        self.button_pause.pack(side="left")
+        self.button_pause["state"] = "disabled"
         self.button_stop = tk.Button(self.button_frame, text="stop", command=self.stop)
         self.button_stop.pack(side="left")
+        self.button_stop["state"] = "disabled"
+        self.button_next = tk.Button(self.button_frame, text="next", command=self.next)
+        self.button_next.pack(side="left")
+        self.button_next["state"] = "disabled"
         self.number_of_times_remaining = tk.Label(self.button_frame)
         self.number_of_times_remaining["text"] = self.repeat_num
         self.number_of_times_remaining.pack(side="left")
+        self.string_var_time_limit = tk.StringVar()
+        self.time_limit = tk.Label(self.button_frame, textvariable=self.string_var_time_limit)
+        self.time_limit["text"] = 0
+        self.time_limit.pack(side="left")
 
         self.photo = tk.PhotoImage()
         self.canvas = tk.Canvas(bg="black", width=self.canvas_width, height=self.canvas_height)
-        self.canvas.place(x=0, y=0)
+        # self.canvas.place(x=0, y=0)
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
         self.canvas.pack(fill=tk.BOTH, expand=tk.YES)
 
         root.bind('<Configure>', self.resize)
 
+    def start(self):
+        if not self.is_ready():
+            return
+
+        self.ready()
+        image = self.get_next_image()
+        self.display_image(image)
+        self.after(self.tick * 1000, self.timer_event)
+
+    def is_ready(self):
+        if not os.path.exists(self.input_directory.get()):
+            print("path not exists.")
+            return False
+        
+        if not self.input_repeat_num.get():
+            print("please input repeat num.")
+            return False
+
+        if self.input_repeat_num.get() == '0':
+            print("please input repeat num.")
+            return False
+
+        try:
+            int(self.input_repeat_num.get())
+        except ValueError:
+            print("please input numeric.")
+            return False
+
+        if not self.input_time_limit.get():
+            print("please input time limit.")
+            return False
+
+        try:
+            int(self.input_time_limit.get())
+        except ValueError:
+            print("please input numeric.")
+            return False
+
+        if not self.input_time_limit.get().isdecimal():
+            print("please input numeric.")
+            return False
+
+        return True
+
     def ready(self):
         self.repeat_num = int(self.input_repeat_num.get())
-        self.index = 0
+        self.index = -1
         self.indexes = [i for i in range(len(self.file_paths))]
+        self.time_limit["text"] = self.input_time_limit["text"]
+        self.current_time = self.input_time_limit["text"]
+        self.string_var_time_limit.set(int(self.input_time_limit.get()))
+        self.number_of_times_remaining["text"] = self.repeat_num
+        self.input_directory["state"] = "disabled"
+        self.input_repeat_num["state"] = "disabled"
+        self.input_time_limit["state"] = "disabled"        
+        self.button_open_directory["state"] = "disabled"
+        self.check_mirror["state"] = "disabled"
+        self.check_random["state"] = "disabled"
+        self.button_start["state"] = "disabled"
+        self.button_stop["state"] = "normal"
+        self.button_next["state"] = "normal"
+        self.button_pause["state"] = "normal"
+
         if self.is_random.get():
             random.shuffle(self.indexes)
 
-    def start(self):
-        self.ready()
-        self.timer_event()
+    def get_next_image(self):
+        self.index = 0 if self.index+1 == len(self.file_paths) else self.index + 1
+        im = Image.open(self.file_paths[self.indexes[self.index]])
+        return im
 
     def timer_event(self):
-        self.number_of_times_remaining["text"] = self.repeat_num
+        self.after_id = self.after(self.tick * 1000, self.timer_event)
+        self.string_var_time_limit.set(int(self.string_var_time_limit.get()) - self.tick)
+        if int(self.string_var_time_limit.get()) > 0:
+            return
+
+        self.string_var_time_limit.set(self.input_time_limit.get())
         self.repeat_num -= 1
-        if self.repeat_num < 0:
+        self.number_of_times_remaining["text"] = self.repeat_num
+        if self.repeat_num <= 0:
             self.save_history()
             self.end()
             return
 
-        self.change_image()
+        image = self.get_next_image()
+        self.display_image(image)
         self.create_history()
-        self.index += 1
-        self.after(int(self.input_time_limit.get()) * 1000, self.timer_event)
 
     def end(self):
         self.photo = tk.PhotoImage()
         self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        pass
+        self.after_cancel(self.after_id)
+        self.repeat_num = 0
+        self.input_directory["state"] = "normal"
+        self.input_repeat_num["state"] = "normal"
+        self.input_time_limit["state"] = "normal"        
+        self.button_open_directory["state"] = "normal"
+        self.check_mirror["state"] = "normal"
+        self.check_random["state"] = "normal"
+        self.button_start["state"] = "normal"
+        self.button_stop["state"] = "disabled"
+        self.button_next["state"] = "disabled"
+        self.button_pause["state"] = "disabled"
 
-    def read_image(self):
+    def open_image(self):
         select_directory_path = filedialog.askdirectory()
-        self.file_paths = glob.glob(select_directory_path + "/*.png")
+        self.file_paths = [p for p in glob.glob(select_directory_path + "/*.*")
+            if re.search('.*\.(png|jpg)', str(p))]
         self.input_directory.delete(0, tk.END)
         self.input_directory.insert(tk.END, select_directory_path)
 
@@ -129,13 +222,13 @@ class Application(tk.Frame):
     def create_history(self):
         self.history.append(self.file_paths[self.indexes[self.index]])
 
-    def change_image(self):
-        im = Image.open(self.file_paths[self.indexes[self.index]])
-        if self.is_mirror.get():
-            im = ImageOps.mirror(im)
-        image_tk = ImageTk.PhotoImage(im)
+    def display_image(self, image):
+        if self.is_mirror.get() and random.randrange(0, 2) == 1:
+            image = ImageOps.mirror(image)
+        image_tk = ImageTk.PhotoImage(image)
         self.photo = image_tk
-        self.canvas.create_image(0, 0, image=image_tk, anchor=tk.NW)
+        
+        self.canvas.create_image(self.master.winfo_width()/2, self.canvas.winfo_height()/2, image=image_tk, anchor=tk.CENTER)
 
     def save_config(self):
         current_settings = []
@@ -143,12 +236,13 @@ class Application(tk.Frame):
         current_settings.append(self.input_repeat_num.get())
         current_settings.append(self.input_time_limit.get())
         current_settings.append(str(self.is_random.get()))
+        current_settings.append(str(self.is_mirror.get()))
 
         fname = tk.filedialog.asksaveasfilename()
         if not fname:
             return
 
-        with open(fname, 'a', encoding="utf-8") as f:
+        with open(fname, 'w', encoding="utf-8") as f:
             f.write(','.join(current_settings))
 
     def open_config(self):
@@ -167,14 +261,31 @@ class Application(tk.Frame):
                 self.input_repeat_num.insert(tk.END, csv[1])
                 self.input_time_limit.insert(tk.END, csv[2])
                 self.is_random.set(csv[3])
+                self.is_mirror.set(csv[4])
 
                 self.file_paths = glob.glob(self.input_directory.get() + "/*.png")
 
     def stop(self):
-        self.repeat_num = 0
-        self.photo = tk.PhotoImage()
-        self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+        self.end()
 
+    def next(self):
+        self.after_cancel(self.after_id)
+        self.string_var_time_limit.set(1)
+        self.timer_event()
+
+    def pause(self):
+        if self.is_puase:
+            self.is_puase = False
+            self.after(self.tick * 1000, self.timer_event)
+            self.button_pause["text"] = "pause"
+            self.button_stop["state"] = "normal"
+            self.button_next["state"] = "normal"
+        else:
+            self.is_puase = True
+            self.after_cancel(self.after_id)
+            self.button_next["state"] = "disabled"
+            self.button_stop["state"] = "disabled"
+            self.button_pause["text"] = "resume"
 
     def save_history(self):
         datetime_format = "%Y%m%d_%H%M%S"
@@ -194,11 +305,9 @@ class Application(tk.Frame):
 
     def open_history(self):
         today = datetime.date.today()
-        print("{}{}{}*{}".format(today.year, today.strftime("%m"), today.strftime("%d"), ".json"))
         file_paths = glob.glob("history/{}{}{}*{}".format(today.year, today.strftime("%m"), today.strftime("%d"), ".json"))
         historys = []
         for file_path in file_paths:
-            print(file_path)
             with open(file_path, 'r', encoding="utf-8") as f:
                 json_file = json.load(f)
                 history = History()
@@ -211,8 +320,6 @@ class Application(tk.Frame):
         sum_repeat_num = 0
         for h in historys:
             sum_repeat_num += int(h.repeat_num)
-
-        print(sum_repeat_num)
 
     def report(self):
         today = datetime.date.today()
@@ -231,8 +338,6 @@ class Application(tk.Frame):
         sum_repeat_num = 0
         for h in historys:
             sum_repeat_num += int(h.repeat_num)
-
-        print(sum_repeat_num)
 
 root = tk.Tk()
 root.state('zoomed')
